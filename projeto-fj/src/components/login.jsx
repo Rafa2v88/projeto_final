9115357070685eb1bd55730305b9a7e3a0b0bb66
Rafa2v88 <r.rodrigues_g@hotmail.com>
2022-10-07 15:38:30 +0100

    A

--- projeto-fj/src/components/login.jsx
+++ projeto-fj/src/components/login.jsx
@@ -1,12 +1,21 @@
-export default function Login(){
+import index from '../../pages/index.jsx'
+import styles from '../../styles/Login.module.css'
+export default function Login() {
     return (
-        <div className="main">
-            <div>
-                <form>
-                    <input></input>
-                    <input></input>
-                </form>
+        <div className={styles.container}>
+            <div class="form-container sign-in-container">
+                <form action="#">
 
+                    <input type="email" placeholder="Email" />
+                    <p><input type="password" placeholder="Password" /></p>
+                    <a href="#">Esqueceu sua senha?</a>
+                    <p><button className={styles.button}>Login</button></p>
+                </form>
+            </div>
+            <div class="overlay-panel overlay-right">
+                <h1>Fast Job</h1>
+                <p>Aqui começa sua jornada, ofereça vagas ou encontre vagas para você</p>
+                <button class="ghost" id="signUp">Registrar</button>
             </div>
         </div>
 
