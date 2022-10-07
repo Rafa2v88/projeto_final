db4b70962fcb08be50cd7d2f3e96ce4925b273c0
Rafa2v88 <r.rodrigues_g@hotmail.com>
2022-10-07 16:19:55 +0100

    fiz login caralho

--- projeto-fj/src/components/login.jsx
+++ projeto-fj/src/components/login.jsx
@@ -3,20 +3,22 @@ import styles from '../../styles/Login.module.css'
 export default function Login() {
     return (
         <div className={styles.container}>
-            <div class="form-container sign-in-container">
+
+            <div className={styles.form}>
                 <form action="#">
 
                     <input type="email" placeholder="Email" />
                     <p><input type="password" placeholder="Password" /></p>
                     <a href="#">Esqueceu sua senha?</a>
-                    <p><button className={styles.button}>Login</button></p>
+                    <p><button className={styles.button2}>Login</button></p>
                 </form>
             </div>
-            <div class="overlay-panel overlay-right">
+            <div className={styles.div2}>
                 <h1>Fast Job</h1>
                 <p>Aqui começa sua jornada, ofereça vagas ou encontre vagas para você</p>
-                <button class="ghost" id="signUp">Registrar</button>
+                <button className={styles.ghost2}>Registrar</button>
             </div>
+
         </div>
 
     )
